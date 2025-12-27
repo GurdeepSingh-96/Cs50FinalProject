6fc6b5d97fffade28c17df2b91ffa767296a7fe5
GurdeepSingh-96 <96gsingh@gmail.com>
2025-12-27 13:13:08 +0100

    completed python backend

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify, request
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/weather")
+def get_weather():
+    # get the city
+    city = request.args.get("city")
+
+    # check anf handle in case miss the city
+    if not city:
+        return jsonify({"error":"Please enter a city name"}), 400
+    
+    # get API Key
+    api_key = os.getenv("WHEATER_API_KEY")
+
+    # API call
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    
+    return jsonify(response.json())
